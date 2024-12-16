d90e2de77ce62a824f9eb04ad70b4ecd1ca7be56
David Chen <darkwei233@gmail.com>
2024-12-16 14:51:05 -0800

    yazi keybind update

--- lua/config/plugins/yazi.lua
+++ lua/config/plugins/yazi.lua
@@ -18,7 +18,7 @@ return {
 			open_file_in_vertical_split = '<c-v>',
 			open_file_in_horizontal_split = '<c-x>',
 			open_file_in_tab = '<c-t>',
-			grep_in_directory = '<c-s>',
+			grep_in_directory = '<c-f>',
 			replace_in_directory = '<c-r>',
 			cycle_open_buffers = '<tab>',
 			copy_relative_path_to_selected_files = '<c-y>',
@@ -26,4 +26,3 @@ return {
 		},
 	},
 }
-
